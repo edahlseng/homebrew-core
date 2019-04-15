106bf5e1207bcfbc2f66d8ce7160adbe1b78ad66
Eric Dahlseng <edahlseng@yahoo.com>
2019-04-14 21:44:17 -0700

    feat: Update notes-organizer

--- Formula/notes-organizer.rb
+++ Formula/notes-organizer.rb
@@ -3,9 +3,9 @@ require "language/node"
 class NotesOrganizer < Formula
   desc "Notes organizer tool"
   homepage "https://github.com/edahlseng/notes-organizer"
-  url "https://registry.npmjs.org/@eric.dahlseng/notes-organizer/-/notes-organizer-0.1.0.tgz"
-  version "0.1.0"
-  sha256 "7f20fffdcebd29e4231a09229c646c23039b7be311d5198aa7fc5f1ee8c1a683"
+  url "https://registry.npmjs.org/@eric.dahlseng/notes-organizer/-/notes-organizer-0.1.1.tgz"
+  version "0.1.1"
+  sha256 "86f7cf88189e6bf2d82170c0774cea44eab4ed5292cb7ba93f85b76c52029688"
 
   depends_on "node"
 
